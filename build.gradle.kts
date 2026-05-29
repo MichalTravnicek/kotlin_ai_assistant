af1cf56eb2e35fd5c2d0d67651f2f5570fb9a145
Michal Travnicek <177191047+MichalTravnicek@users.noreply.github.com>
2026-05-29 15:32:24 +0200

    Build gradle fix3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.10"
-    id("io.ktor.plugin") version "3.0.3"
+    id("io.ktor.plugin") version "3.1.0"
     kotlin("plugin.serialization") version "1.9.22"
 }
 
